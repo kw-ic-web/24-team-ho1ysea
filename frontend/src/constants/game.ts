cc545d8c053c42cbc53c18e7559ff391dcdd99e9
NARARIA03 <hyeonseong0305@gmail.com>
2024-10-09 18:47:09 +0900

    Feat: 논리적 게임 좌표와 게임 화면을 맞추는 훅 구현
    이를 통해 사용자의 기기 해상도에 관계없이 /constants/game.ts에 명시된
    WORLD_W, WORLD_H, WORLD_RATIO에 맞는 최대 화면 크기로 렌더링 할 수 있게
    된다.

---
+++ frontend/src/constants/game.ts
@@ -0,0 +1,3 @@
+export const WORLD_W = 1280; // 게임 월드 가로 크기
+export const WORLD_H = 720; // 게임 월드 세로 크기
+export const WORLD_RATIO = WORLD_W / WORLD_H; // 게임 월드 비율 (16:9)
--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -1,3 +1,20 @@
+import { useCanvasInit } from "@hooks/useCanvasInit";
+
 export default function GamePage() {
-  return <p className="text-3xl">GamePage 입니다</p>;
+  const canvasRef = useCanvasInit();
+
+  return (
+    <div>
+      <canvas
+        ref={canvasRef}
+        style={{
+          display: "block",
+          margin: "0 auto",
+          border: "solid 1px black",
+        }}
+      >
+        CANVAS가 지원되는 브라우저를 사용하세요
+      </canvas>
+    </div>
+  );
 }
