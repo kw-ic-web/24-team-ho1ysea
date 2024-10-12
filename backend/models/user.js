3680101c4db6e81b5f8c9755845b43c145a5a099
thingsub <paularam0527@naver.com>
2024-10-12 10:43:54 +0900

    대략적인 폴더 구조 + js로 user 스키마 생성(비번 hash 고려 X)

---
+++ backend/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+// 사용자 스키마 설계
+const userSchema = new mongoose.Schema({
+    id: { type: String, required: true, unique: true },
+    pw: { type: String, required: true, set: encryptPassword },  // 저장 시 암호화
+    nickName: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },  // 기본값 false
+    topRate: { type: Number, default: 0 },
+    coin: { type: Number, default: 0 },
+    countPlay: { type: Number, default: 0 }
+});
+
+// 사용자 모델 생성
+const User = mongoose.model('User', userSchema);
+
+// 내보내기
+module.exports = User;
