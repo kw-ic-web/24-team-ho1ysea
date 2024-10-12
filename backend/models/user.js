17a4dbaeb0847114d6cf709eeca734a9da55d72c
thingsub <paularam0527@naver.com>
2024-10-12 11:05:08 +0900

    Chore: 프로젝트 폴더 구조 설정

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,11 +3,7 @@ const mongoose = require('mongoose');
 // 사용자 스키마 설계
 const userSchema = new mongoose.Schema({
     id: { type: String, required: true, unique: true },
-<<<<<<< HEAD
     pw: { type: String, required: true }, 
-=======
-    pw: { type: String, required: true, set: encryptPassword },  // 저장 시 암호화
->>>>>>> 3680101c4db6e81b5f8c9755845b43c145a5a099
     nickName: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },  // 기본값 false
     topRate: { type: Number, default: 0 },
