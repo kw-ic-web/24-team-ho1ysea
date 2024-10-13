578c7be7f8939bf1a1591e61a4ab0c86c4baafa3
thingsub <paularam0527@naver.com>
2024-10-13 15:20:58 +0900

    Feat : 유저 정보 조회(/me) API 및 관련 로직 구현

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,12 +5,16 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const userController = require('../controllers/userController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 // 회원가입 엔드포인트
 router.post('/signup', userController.signupUser);
 
 // 로그인 엔드포인트
 router.post('/login', userController.loginUser);
+
+// 유저 정보 조회 엔드포인트(토큰 필요)
+router.get('/me', authMiddleware, userController.getUserInfo);
 
 // 닉네임 중복 체크 엔드포인트
 //router.get('/check-nickname', userController.checkNicknameAvailability);
