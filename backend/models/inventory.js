e7314022d7e18937cbd80174e555a6d10f816f1e
thingsub <paularam0527@naver.com>
2024-10-14 11:36:49 +0900

    Feat : 보유 아이템 조회 목적으로 inventory 스키마 추가

---
+++ backend/models/inventory.js
@@ -0,0 +1,30 @@
+// models/inventory.js
+const mongoose = require('mongoose');
+
+// 인벤토리 스키마 설계
+const inventorySchema = new mongoose.Schema({
+    userId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    }, 
+    items: [ 
+        { 
+            itemId: { 
+                type: mongoose.Schema.Types.ObjectId, 
+                ref: 'Item', 
+                required: true 
+            },
+            quantity: { 
+                type: Number, 
+                default: 1 
+            }
+        }
+    ]
+});
+
+// 인벤토리 모델 생성
+const Inventory = mongoose.model('Inventory', inventorySchema);
+
+// 내보내기
+module.exports = Inventory;
