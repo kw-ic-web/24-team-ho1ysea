72e429f2bac4e9c16f6cc577f1b605e4a1b0f6a3
JSH <king601143@gmail.com>
2024-10-19 02:10:02 +0900

    Feat : Add Item, Inventory models

---
+++ backend/models/inventory.js
@@ -0,0 +1,15 @@
+// models/inventory.js
+
+const mongoose = require("mongoose");
+
+const inventorySchema = new mongoose.Schema({
+  userid: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // 회원 데이터와 연결하는 ID
+  items: [
+    {
+      itemId: { type: String, required: true }, // 아이템 아이디
+      quantity: { type: Number, default: 1 }, // 수량
+    },
+  ],
+});
+
+module.exports = mongoose.model("Inventory", inventorySchema);
---
+++ backend/models/item.js
@@ -0,0 +1,13 @@
+// models/item.js
+
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema({
+  itemId: { type: String, required: true, unique: true }, // 아이템 아이디
+  itemName: { type: String, required: true }, // 아이템 이름
+  description: { type: String }, // 설명
+  cost: { type: Number, required: true }, // 가격
+  image: { type: String }, // S3 주소 (필수 아님)
+});
+
+module.exports = mongoose.model("Item", itemSchema);
