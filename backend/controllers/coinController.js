9025919bd2b3046eabbce1351e2cc40330f10260
NARARIA03 <hyeonseong0305@gmail.com>
2024-10-20 04:43:42 +0900

    Chore: FE 개발 편의를 위해 추가했던 API 제거

--- backend/controllers/coinController.js
+++ backend/controllers/coinController.js
@@ -20,19 +20,19 @@ exports.getMyCoin = async (req, res) => {
 };
 
 // FE 개발 편의를 위해 임의로 구현한 API, 추후 반드시 제거!
-exports.testIncCoin = async (req, res) => {
-  const userId = req.user._id;
-  try {
-    const user = await User.findById(userId);
+// exports.testIncCoin = async (req, res) => {
+//   const userId = req.user._id;
+//   try {
+//     const user = await User.findById(userId);
 
-    if (!user) {
-      return res.status(404).json({ message: "사용자를 찾을 수 없습니다." });
-    }
-    user.coin += 1000;
-    await user.save();
-    return res.status(200).json({ message: "정상적으로 코인 증가" });
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("서버 오류");
-  }
-};
+//     if (!user) {
+//       return res.status(404).json({ message: "사용자를 찾을 수 없습니다." });
+//     }
+//     user.coin += 1000;
+//     await user.save();
+//     return res.status(200).json({ message: "정상적으로 코인 증가" });
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send("서버 오류");
+//   }
+// };
