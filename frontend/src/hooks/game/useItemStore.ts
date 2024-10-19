5ca53680e80f0645339ba1709e8d3457aeb0c210
NARARIA03 <hyeonseong0305@gmail.com>
2024-10-20 02:47:16 +0900

    Feat: 토큰이 만료되거나, 없는 경우 예외처리 추가
    상점에 들어갈 때, 아이템을 구매할 때, 아이템을 판매할 때
    토큰이 있는지, 토큰이 유효한지를 확인하고 이에 맞게 alert 띄우도록 구현.
    토큰이 없거나 만료되었다면 로그인 페이지로 리다이렉트

--- frontend/src/hooks/game/useItemStore.ts
+++ frontend/src/hooks/game/useItemStore.ts
@@ -11,12 +11,13 @@ import {
 import { getLocalStorage } from "@utils/localStorage";
 import axios, { isAxiosError } from "axios";
 import { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 /**
  * @description 상점 아이템, 내 보유 아이템, 아이템 구매, 아이템 판매 등을 위한 함수 / state를 반환하는 커스텀 훅
  */
-
 export const useItemStore = (isOpen: boolean) => {
+  const navigate = useNavigate();
   const [storeItems, setStoreItems] = useState<StoreItems>([]);
   const [myItems, setMyItems] = useState<MyItems>([]);
   const [currency, setCurrency] = useState<Currency>({
@@ -24,51 +25,86 @@ export const useItemStore = (isOpen: boolean) => {
     trash: 0,
   });
 
-  const handleBuyItem = useCallback(async (item: StoreItems[0]) => {
-    try {
-      const { itemId, itemName } = item;
-      const token = getLocalStorage("token");
-      if (!token) return;
+  const handleBuyItem = useCallback(
+    async (item: StoreItems[0]) => {
+      try {
+        const { itemId, itemName } = item;
+        const token = getLocalStorage("token");
+        if (!token) {
+          alert("먼저 로그인 해주세요.");
+          navigate("/");
+          return;
+        }
 
-      await buyStoreItemApi(itemId, 1, token);
-      alert(itemName + " 구매 성공!");
+        await buyStoreItemApi(itemId, 1, token);
+        alert(itemName + " 구매 성공!");
 
-      const myItemsData = await myItemsApi(token).then((res) => res.data);
-      const myCoinData = await myCoinApi(token!).then((res) => res.data.coin);
-      setMyItems(myItemsData);
-      setCurrency((prev) => ({ ...prev, coin: myCoinData }));
-    } catch (err) {
-      if (isAxiosError(err)) {
-        console.error(err);
-        alert("아이템 구매에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.");
+        const myItemsData = await myItemsApi(token).then((res) => res.data);
+        const myCoinData = await myCoinApi(token!).then((res) => res.data.coin);
+        setMyItems(myItemsData);
+        setCurrency((prev) => ({ ...prev, coin: myCoinData }));
+      } catch (err) {
+        if (isAxiosError(err)) {
+          console.error(err);
+          if (err.status === 401) {
+            alert("세션이 만료되었습니다. 다시 로그인 해주세요.");
+            navigate("/");
+          } else {
+            alert(
+              "아이템 구매에 문제가 발생했습니다. 잠시 후 다시 시도해주세요."
+            );
+          }
+        }
       }
-    }
-  }, []);
+    },
+    [navigate]
+  );
 
-  const handleSellItem = useCallback(async (item: MyItems[0]) => {
-    try {
-      const { itemId, itemName } = item;
-      const token = getLocalStorage("token");
-      if (!token) return;
+  const handleSellItem = useCallback(
+    async (item: MyItems[0]) => {
+      try {
+        const { itemId, itemName } = item;
+        const token = getLocalStorage("token");
+        if (!token) {
+          alert("먼저 로그인 해주세요.");
+          navigate("/");
+          return;
+        }
 
-      await sellStoreItemApi(itemId, 1, token);
-      alert(itemName + " 판매 성공!");
+        await sellStoreItemApi(itemId, 1, token);
+        alert(itemName + " 판매 성공!");
 
-      const myItemsData = await myItemsApi(token).then((res) => res.data);
-      const myCoinData = await myCoinApi(token!).then((res) => res.data.coin);
-      setMyItems(myItemsData);
-      setCurrency((prev) => ({ ...prev, coin: myCoinData }));
-    } catch (err) {
-      if (isAxiosError(err)) {
-        console.error(err);
-        alert("아이템 판매에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.");
+        const myItemsData = await myItemsApi(token).then((res) => res.data);
+        const myCoinData = await myCoinApi(token!).then((res) => res.data.coin);
+        setMyItems(myItemsData);
+        setCurrency((prev) => ({ ...prev, coin: myCoinData }));
+      } catch (err) {
+        if (isAxiosError(err)) {
+          console.error(err);
+          if (err.status === 401) {
+            alert("세션이 만료되었습니다. 다시 로그인 해주세요.");
+            navigate("/");
+          } else {
+            alert(
+              "아이템 판매에 문제가 발생했습니다. 잠시 후 다시 시도해주세요."
+            );
+          }
+        }
       }
-    }
-  }, []);
+    },
+    [navigate]
+  );
 
   useEffect(() => {
-    const fetchData = async (token: string) => {
+    const fetchData = async () => {
       try {
+        const token = getLocalStorage("token");
+        if (!token) {
+          alert("먼저 로그인 해주세요.");
+          navigate("/");
+          return;
+        }
+
         const storeItemsData = await allStoreItemsApi().then((res) => res.data);
         const myItemsData = await myItemsApi(token).then((res) => res.data);
         const myCoinData = await myCoinApi(token).then((res) => res.data.coin);
@@ -77,16 +113,19 @@ export const useItemStore = (isOpen: boolean) => {
         setCurrency((prev) => ({ ...prev, coin: myCoinData }));
       } catch (err) {
         if (isAxiosError(err)) {
-          console.error(err.response);
+          console.error(err);
+          if (err.status === 401) {
+            alert("세션이 만료되었습니다. 다시 로그인 해주세요.");
+            navigate("/");
+          }
         }
       }
     };
 
-    const token = getLocalStorage("token");
-    if (isOpen && token) {
-      fetchData(token);
+    if (isOpen) {
+      fetchData();
     }
-  }, [isOpen]);
+  }, [isOpen, navigate]);
 
   // 추후 제거!!
   const handleIncCost = useCallback(async () => {
