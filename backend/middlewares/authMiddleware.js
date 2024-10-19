8c7c4af9f9573fcdb6e4040281326496dc5b8852
thingsub <paularam0527@naver.com>
2024-10-19 19:28:37 +0900

    Fix : 관리자용 코드 일부 추가

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,8 +1,9 @@
 // middlewares/authMiddleware.js
 
+const mongoose = require("mongoose");
 require("dotenv").config();
 // .env 파일의 내용을 로드 (cf. '.env' 내용 참조하는 파일들은 명시 필요)
-
+const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
 // 이걸 넘겨준 엔드포인트는 인증된 사용자만 접근할 수 있음
@@ -11,33 +12,32 @@ const jwt = require("jsonwebtoken");
 
 module.exports = async function (req, res, next) {
   const token = req.header("Authorization")?.split(" ")[1];
-
   if (!token) {
     return res
       .status(401)
-      .json({ msg: "토큰이 없습니다. 인증이 거부되었습니다." });
+      .json({ message: "토큰이 없습니다. 인증이 거부되었습니다." });
   }
 
-  // 토큰이 있다면, isAdmin 여부로 admin이나 login 페이지로 보내주자.
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); // 열려라 참깨
-   // req.user = { id: decoded.userId };
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId);
 
-   const user = await User.findById(decoded.userId); // 사용자 정보를 DB에서 찾기
-    if (!user) { // 예외처리 추가
+    if (!user) {
       return res.status(401).json({ message: "사용자를 찾을 수 없습니다." });
     }
 
     req.user = user; // 요청 객체에 사용자 정보 추가
 
-    if (user.isAdmin) { // isAdmin : TRUE
-      return next(); // admin 페이지로
-    }     
-    else { // isAdmin : FALSE
-      return next(); // 개인 페이지로
+    // 사용자 권한에 따라 다른 상태 코드 설정
+    if (user.isAdmin) {
+      res.status(204); // 관리자일 경우
+    } else {
+      res.status(200); // 일반 사용자일 경우
     }
-  } 
-  catch (err) {
-    res.status(401).json({ msg: "유효하지 않은 토큰입니다." });
+
+    next(); //  미들웨어 1개만 유지  & (현성이 요청대로) 상이한 응답코드  
+
+  } catch (err) {
+    res.status(401).json({ message: "유효하지 않은 토큰입니다." });
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use("/api/user", require("./routes/userRoutes"));
 app.use("/api/store", require("./routes/storeRoutes"));
 app.use("/api/coin", require("./routes/coinRoutes"));
 app.use("/api/item", require("./routes/itemRoutes"));
-
+app.use('/api/admin',require("./routes/adminRoutes"));
 
 // 매일 자정에 탈퇴 처리 함수 실행
 // cron.schedule('0 0 * * *', userController.processAccountCancellation);   // 신기해서 넣어봤음(탈퇴처리 + 탈퇴예약 2가지 고려해야 함)
