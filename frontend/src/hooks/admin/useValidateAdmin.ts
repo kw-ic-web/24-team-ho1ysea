552bc285e5ace2f2416c2d5d1b769862789f6862
NARARIA03 <hyeonseong0305@gmail.com>
2024-10-22 15:58:50 +0900

    Feat: 헤더 구현 / 어드민 여부 확인 전까지 렌더링 X
    useValidateAdmin 훅에서 isLoading 값을 반환하도록 해서 어드민 여부를 검
    사하기 전까지는 컴포넌트들을 렌더링하지 않도록 수정했다.

--- frontend/src/hooks/admin/useValidateAdmin.ts
+++ frontend/src/hooks/admin/useValidateAdmin.ts
@@ -2,13 +2,14 @@ import { isAdminApi } from "@apis/adminRestful";
 import { useToastStore } from "@store/toastStore";
 import { getLocalStorage } from "@utils/localStorage";
 import { isAxiosError } from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 /**
  * @description 사용자가 admin인지 확인하고, admin이 아닌 경우 적절한 안내와 함께 다른 페이지로 던지는 커스텀 훅
  */
 export const useValidateAdmin = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const navigate = useNavigate();
   const showToast = useToastStore((state) => state.showToast);
 
@@ -16,6 +17,7 @@ export const useValidateAdmin = () => {
     const validateAdmin = async (token: string) => {
       try {
         const message = await isAdminApi(token).then((res) => res.data.message);
+        setIsLoading(false);
         showToast(message);
       } catch (err) {
         if (isAxiosError<{ message: string }>(err) && err.response) {
@@ -23,12 +25,15 @@ export const useValidateAdmin = () => {
 
           if (err.response.status === 401) {
             navigate("/");
+            return;
           } else if (err.response.status === 403) {
             navigate("/game");
+            return;
           }
         } else {
           showToast("알 수 없는 에러가 발생했습니다.");
           navigate("/");
+          return;
         }
       }
     };
@@ -41,4 +46,6 @@ export const useValidateAdmin = () => {
       navigate("/");
     }
   }, [navigate, showToast]);
+
+  return isLoading;
 };
--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -1,13 +1,21 @@
+import AdminHeader from "@components/admin/AdminHeader";
 import ViewUsers from "@components/admin/ViewUsers";
+import Loading from "@components/common/Loading";
 import { useValidateAdmin } from "@hooks/admin/useValidateAdmin";
 
 export default function AdminPage() {
   // 사용자가 어드민인지 확인하고 처리 수행
-  useValidateAdmin();
+  const isLoading = useValidateAdmin();
 
+  // 관리자 권한이 확인되기 전까지 페이지 내용 렌더링을 지연
+  if (isLoading) {
+    return <Loading isLoading={true} />;
+  }
+
+  // 권한 검증 완료 후에만 관리자 페이지를 렌더링
   return (
-    <div className="bg-sky-100 text-gray-800">
-      <p>어드민 페이지 임니다</p>
+    <div className="text-gray-800">
+      <AdminHeader />
       <ViewUsers />
     </div>
   );
