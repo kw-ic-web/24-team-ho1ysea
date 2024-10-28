f124acfb98001def92470f4c921e4152e0180916
NARARIA03 <hyeonseong0305@gmail.com>
2024-10-28 10:57:20 +0900

    Feat: 게임 설정 변경 / 전체 유저 확인 라우팅 처리
    state 기반으로 컴포넌트를 다르게 보여주도록 했음 <- 라우터 이용 X

--- frontend/src/components/admin/AdminHeader.tsx
+++ frontend/src/components/admin/AdminHeader.tsx
@@ -1,17 +1,37 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
-export default function AdminHeader() {
+interface Props {
+  compSelector: "viewUsers" | "editSetting";
+  setCompSelector: React.Dispatch<
+    React.SetStateAction<"viewUsers" | "editSetting">
+  >;
+}
+
+export default function AdminHeader({ compSelector, setCompSelector }: Props) {
   return (
     <header className="w-full p-4 shadow-sm">
       <nav className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold">바다 이야기 Admin</h1>
         <ul className="flex space-x-6">
+          <li
+            className="hover:text-gray-400 transition-colors cursor-pointer"
+            onClick={() =>
+              compSelector === "viewUsers"
+                ? setCompSelector("editSetting")
+                : setCompSelector("viewUsers")
+            }
+          >
+            {compSelector === "viewUsers"
+              ? "게임 설정 변경하기"
+              : "전체 유저 정보 확인"}
+          </li>
           <li>
             <Link
               to="/game"
               className="p-2 hover:text-gray-400 transition-colors"
             >
-              게임 페이지로 돌아가기
+              게임으로 돌아가기
             </Link>
           </li>
         </ul>
--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -2,10 +2,14 @@ import AdminHeader from "@components/admin/AdminHeader";
 import ViewUsers from "@components/admin/ViewUsers";
 import Loading from "@components/common/Loading";
 import { useValidateAdmin } from "@hooks/admin/useValidateAdmin";
+import { useState } from "react";
 
 export default function AdminPage() {
   // 사용자가 어드민인지 확인하고 처리 수행
   const isLoading = useValidateAdmin();
+  const [compSelector, setCompSelector] = useState<"viewUsers" | "editSetting">(
+    "viewUsers"
+  );
 
   // 관리자 권한이 확인되기 전까지 페이지 내용 렌더링을 지연
   if (isLoading) {
@@ -15,8 +19,11 @@ export default function AdminPage() {
   // 권한 검증 완료 후에만 관리자 페이지를 렌더링
   return (
     <div className="text-gray-800">
-      <AdminHeader />
-      <ViewUsers />
+      <AdminHeader
+        compSelector={compSelector}
+        setCompSelector={setCompSelector}
+      />
+      {compSelector === "viewUsers" && <ViewUsers />}
     </div>
   );
 }
