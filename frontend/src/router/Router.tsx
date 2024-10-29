a70969eeaee6a8b2bbc1436b7e34ba177fef7a78
NARARIA03 <hyeonseong0305@gmail.com>
2024-10-29 11:08:57 +0900

    Refactor: 랜딩 페이지에 중첩 라우팅 적용
    로그인 회원가입 모달창을 state로 관리하고, 랜딩 페이지, 로그인 모달, 회
    원가입 모달 등에는 react-router-dom을 사용하지 않았기 때문에 새로고침 시
    회원가입 모달을 열어뒀어도 다시 닫히는 등의 불편함이 있을 수 있다.
    
    이를 해결하기 위해 관리자 페이지처럼 `/` 내에 `login`과 `register`를 둬서
    중첩 라우팅을 적용하는 방식으로 리팩토링했다.
    
    이 결과 state props drill이 필요 없어졌고, Link 태그와 useNavigate만으로
    해결 가능해졌다. 또 코드가 간단해져서 이제 Router.tsx로 웹페이지 전체 구
    조를 파악한 뒤에 컴포넌트들을 찾아가서 로직을 이해하는 것이 가능해졌다.
    (랜딩 페이지의 조건부 렌더링이 사라짐)

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -4,12 +4,17 @@ import LandingPage from "@pages/LandingPage";
 import AdminPage from "@pages/AdminPage";
 import ViewUsers from "@components/admin/ViewUsers";
 import EditSetting from "@components/admin/EditSetting";
+import LoginModal from "@components/landing/LoginModal";
+import RegisterModal from "@components/landing/RegisterModal";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />}>
+          <Route path="login" element={<LoginModal />} />
+          <Route path="register" element={<RegisterModal />} />
+        </Route>
         <Route path="/game" element={<GamePage />} />
         <Route path="/admin" element={<AdminPage />}>
           <Route path="view-users" element={<ViewUsers />} />
