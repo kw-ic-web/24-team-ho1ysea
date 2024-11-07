77096701e3fb8ef9dcaa7dc800f38fe1e910f4c0
NARARIA03 <hyeonseong0305@gmail.com>
2024-11-08 00:48:40 +0900

    Refactor: Redis 관련 코드를 redisHandler로 이동

--- backend/utils/gameUtils.js
+++ backend/utils/gameUtils.js
@@ -1,6 +1,14 @@
 // backend/utils/gameUtils.js
 
 const { redisClient } = require("../config/db");
+const {
+  updateTrashPositions,
+  getTrashPositions,
+  updateObstaclePositions,
+  getObstaclePositions,
+  updateItemPositions,
+  getItemPositions,
+} = require("./redisHandler");
 const { v4: uuidv4 } = require("uuid");
 // 이거 고유id를 부여하는 방법 중 하나라, 좀 더 편한 대체 방안 있으면 수정해도 좋아요!
 // Mongoose 모델 임포트
@@ -42,17 +50,12 @@ exports.generateTrash = async () => {
   const trashId = randomTrash.trashId; // Trash 모델의 trashId 필드 사용
   const position = generateRandomPosition();
 
-  const newTrash = { objectId, trashId, position };
+  // Redis에 업데이트
+  await updateTrashPositions(objectId, trashId, position);
 
-  // 직렬화해서 Redis에 저장
-  await redisClient.lPush("trashPositions", JSON.stringify(newTrash));
-
-  // 전체 쓰레기 리스트 반환
-  // redisClient.lRange(key, start, stop) 메서드를 사용하여,리스트를 가져옴
-  // 매개변수 0은 시작, -1은 끝을 의미함.
-  const trashList = await redisClient.lRange("trashPositions", 0, -1);
-  // 아이템 개별로 파싱해서 값 리턴
-  return trashList.map((item) => JSON.parse(item));
+  // 업데이트된 전체 쓰레기 데이터 가져오기
+  const trashList = await getTrashPositions();
+  return trashList;
 };
 
 // 방해요소 생성 함수
@@ -74,14 +77,12 @@ exports.generateObstacle = async () => {
   const obstacleId = randomObstacle.obstacleId; // Obstacle 모델의 obstacleId 필드 사용
   const position = generateRandomPosition();
 
-  const newObstacle = { objectId, obstacleId, position };
-
   // Redis에 저장
-  await redisClient.lPush("obstaclePositions", JSON.stringify(newObstacle));
+  await updateObstaclePositions(objectId, obstacleId, position);
 
-  // 전체 방해요소 리스트 반환
-  const obstacleList = await redisClient.lRange("obstaclePositions", 0, -1);
-  return obstacleList.map((item) => JSON.parse(item));
+  // 업데이트된 전체 방해요소 데이터 가져오기
+  const obstacleList = await getObstaclePositions();
+  return obstacleList;
 };
 
 // 아이템 생성 함수
@@ -103,12 +104,10 @@ exports.generateItem = async () => {
   const itemId = randomItem.itemId; // Item 모델의 itemId 필드 사용
   const position = generateRandomPosition();
 
-  const newItem = { objectId, itemId, position };
+  // Redis에 업데이트
+  await updateItemPositions(objectId, itemId, position);
 
-  // Redis에 저장
-  await redisClient.lPush("itemPositions", JSON.stringify(newItem));
-
-  // 전체 아이템 리스트 반환
-  const itemList = await redisClient.lRange("itemPositions", 0, -1);
-  return itemList.map((item) => JSON.parse(item));
+  // 업데이트된 전체 아이템 데이터 가져오기
+  const itemList = await getItemPositions();
+  return itemList;
 };
--- backend/utils/redisHandler.js
+++ backend/utils/redisHandler.js
@@ -1,12 +1,12 @@
 const { redisClient } = require("../config/db");
 
+// 연결 해제된 플레이어를 플레이어 포지션 Redis에서 제거하고, 남은 전체 데이터 리턴
 exports.removeUserData = async (userId) => {
-  // 연결 해제된 플레이어를 Redis에서 제거
-  await redisClient.hDel("player_positions", userId);
+  await redisClient.hDel("playerPositions", userId);
   console.log(`사용자 ${userId} 위치 정보가 Redis에서 제거되었습니다.`);
 
   // Redis에 저장되어있는 플레이어들 정보 가져오기
-  const players = await redisClient.hGetAll("player_positions");
+  const players = await redisClient.hGetAll("playerPositions");
   const broadcastData = Object.keys(players).map((userId) => ({
     ...JSON.parse(players[userId]),
   }));
@@ -14,19 +14,64 @@ exports.removeUserData = async (userId) => {
   return broadcastData;
 };
 
+// userId를 키로 사용해 플레이어 포지션 Redis에 저장/업데이트 후, 전체 데이터 리턴
 exports.updateUserData = async (userId, nickName, position) => {
-  // userId를 키로 사용해 유저 데이터를 Redis에 저장
   await redisClient.hSet(
-    "player_positions",
+    "playerPositions",
     userId,
     JSON.stringify({ userId, nickName, position })
   );
 
   // 저장된 모든 플레이어 정보를 가져오기
-  const players = await redisClient.hGetAll("player_positions");
+  const players = await redisClient.hGetAll("playerPositions");
   const broadcastData = Object.keys(players).map((userId) => ({
     ...JSON.parse(players[userId]),
   }));
 
   return broadcastData;
+};
+
+// 새로 생성한 쓰레기를 Redis에 저장
+exports.updateTrashPositions = async (objectId, trashId, position) => {
+  const newTrash = { objectId, trashId, position };
+  // 직렬화해서 Redis에 저장
+  await redisClient.lPush("trashPositions", JSON.stringify(newTrash));
+  return true;
+};
+
+// 전체 쓰레기 데이터 리턴
+exports.getTrashPositions = async () => {
+  // 전체 쓰레기 리스트 반환
+  // redisClient.lRange(key, start, stop) 메서드를 사용하여,리스트를 가져옴
+  // 매개변수 0은 시작, -1은 끝을 의미함.
+  const trashList = await redisClient.lRange("trashPositions", 0, -1);
+
+  // 아이템 개별로 파싱해서 값 리턴
+  return trashList.map((item) => JSON.parse(item));
+};
+
+// 새로 생성한 방해요소를 Redis에 저장
+exports.updateObstaclePositions = async (objectId, obstacleId, position) => {
+  const newObstacle = { objectId, obstacleId, position };
+  await redisClient.lPush("obstaclePositions", JSON.stringify(newObstacle));
+  return true;
+};
+
+// 전체 방해요소 데이터 리턴
+exports.getObstaclePositions = async () => {
+  const obstacleList = await redisClient.lRange("obstaclePositions", 0, -1);
+  return obstacleList.map((item) => JSON.parse(item));
+};
+
+// 새로 생성한 아이템(게임 맵에 떠있는)을 Redis에 저장
+exports.updateItemPositions = async (objectId, itemId, position) => {
+  const newItem = { objectId, itemId, position };
+  await redisClient.lPush("itemPositions", JSON.stringify(newItem));
+  return true;
+};
+
+// 전체 아이템(게임 맵에 떠있는) 데이터 리턴
+exports.getItemPositions = async () => {
+  const itemList = await redisClient.lRange("itemPositions", 0, -1);
+  return itemList.map((item) => JSON.parse(item));
 };
