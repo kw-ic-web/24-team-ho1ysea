ee211f5db519445774bc53e0c7f306249fce52b2
NARARIA03 <hyeonseong0305@gmail.com>
2024-11-08 02:30:27 +0900

    Feat: 방해 요소 (상어, 해파리) 프론트와 연결 완료

--- frontend/src/types/obstacleType.ts
+++ frontend/src/types/obstacleType.ts
@@ -2,6 +2,6 @@ import { ObjectPos } from "./GameType";
 
 export type Obstacle = {
   objectId: string;
-  obstacleId: string; // 추후 확실한 타입으로 내로잉 ("해파리" | "상어" 이런식으로)
+  obstacleId: "obstacle001" | "obstacle002";
   position: ObjectPos; // { x: number, y:number }
 };
