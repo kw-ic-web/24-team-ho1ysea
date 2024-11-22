bb1bc4e5b34811b00aeccfea0402595cb23fd648
JSH <king601143@gmail.com>
2024-11-23 03:40:31 +0900

    Feat : 상어 및 해파리 충돌 이벤트 처리

--- backend/sockets/eventHandler/playerMove.js
+++ backend/sockets/eventHandler/playerMove.js
@@ -1,10 +1,11 @@
 // backend/sockets/eventHandler/playerMove.js
 
+const obstacle = require("../../models/obstacle");
 const {
   checkCollision,
   updateUserTrashAmount,
 } = require("../../utils/gameUtils");
-const { updateUserData } = require("../../utils/redisHandler");
+const { updateUserData, removeUserData } = require("../../utils/redisHandler");
 
 /**
  * @description 특정 플레이어가 이동하면, gameRoom 내의 모든 플레이어들에게 브로드캐스트
@@ -44,8 +45,38 @@ exports.playerMove = (io, socket) => {
       console.log("충돌한 아이템 정보:", collisionResult.id);
       socket.emit("getItem", collisionResult.id);
       // 프론트에서는 이 이벤트를 수신하면 item 습득 API로 다시 요청을 날리고, 인벤토리 정보를 새로고침함
-    } else if (collisionResult && collisionResult.type === "obstacle") {
+    } else if (collisionResult.type === "obstacle") {
+      // 전체 플레이어에게 장애물이 사라졌음을 알림
       io.to("gameRoom").emit("collisionObstacle", collisionResult.data);
+
+      console.log("충돌한 장애물 정보:", collisionResult.id);
+
+      if (collisionResult.id === "obstacle001") {
+        // 상어와 충돌한 경우
+        socket.emit("collisionShark");
+        console.log(`${userId}가 상어와 충돌하여 사망했습니다.`);
+
+        // 먼저 소켓을 gameRoom에서 제거
+        socket.leave("gameRoom");
+
+        // 플레이어를 Redis에서 제거
+        const broadcastData = await removeUserData(userId);
+
+        // 제거한 후, 남은 플레이어들에게 업데이트된 캐릭터 위치를 브로드캐스트
+        io.to("gameRoom").emit("updateCharacterPosition", broadcastData);
+
+        // 플레이어의 게임 데이터를 초기화
+        socket.emit("updateCharacterPosition", []);
+        socket.emit("generateRandomTrash", []);
+        socket.emit("generateRandomObstacle", []);
+        socket.emit("generateRandomItem", []);
+      } else if (collisionResult.id === "obstacle002") {
+        // 해파리와 충돌한 경우
+        socket.emit("collisionJellyfish");
+        console.log(`${userId}가 해파리에 쏘여 어지러워집니다.`);
+      } else {
+        console.log(`${userId}가 미확인 수중 물체와 충돌했습니다! USO`);
+      }
     }
 
     // gameRoom 방 전체에 브로드캐스트
